9039d4337846ff2ec82b7e141f9f4dceee09ad6f
Jack Ogilvie-Richards <66806642+jackogilvierichards@users.noreply.github.com>
2025-02-24 15:24:45 +0000

    updated challenge_01

--- challenge_01.rb
+++ challenge_01.rb
@@ -10,3 +10,12 @@ lines.times do |n|
   puts str
 end
 
+pattern = "++*~~*++*"
+
+pattern_array = pattern.split("")
+
+lines.times do |n|
+  first = pattern_array.shift #take first item in array
+  pattern_array << first #appends the first value onto the end of the pattern array
+  puts pattern_array.join #returns the string version of the array (joined together to make a string again after being split)
+end
