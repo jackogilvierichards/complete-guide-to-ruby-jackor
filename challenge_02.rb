2febd310cfed9ed07a7b66dc548456b4418c1a38
Jack Ogilvie-Richards <66806642+jackogilvierichards@users.noreply.github.com>
2025-02-25 10:17:20 +0000

    finished challenge_02.rb

--- challenge_02.rb
+++ challenge_02.rb
@@ -10,3 +10,47 @@
 
 [*1..10].shuffle.first #range we are interested in, return as array type, shuffle the order of values in array,  
 rand(100) # returns an integer in 0..99
+
+puts "----------------------------------"
+puts "Welcome to The Ruby Guessing Game!"
+puts "----------------------------------"
+puts "What's your name, thou noble sire?"
+puts "----------------------------------"
+response_1 = gets.chomp
+sleep 1
+puts "----------------------------------"
+puts "Hello, #{response_1}, and welcome!"
+puts "----------------------------------"
+puts "The rules are as follows: I will"
+puts "pick any number between 1-10 and"
+puts "you will get 3 tries to guess it!"
+puts "----------------------------------"
+puts "----------------------------------"
+puts "Okay, here goes nothing - game on!"
+puts "----------------------------------"
+rand_num = [*1..10].shuffle.first
+sleep 1
+puts "Can you guess what number I chose?"
+puts rand_num
+puts "----------------------------------"
+
+
+3.times do |n|
+  guess = gets.chomp.to_i
+  if guess == rand_num
+    puts "----------------------------------"
+    puts "Congratulations! You are correct! "
+    puts "       Thanks for playing!        "
+    puts "----------------------------------"
+    exit!
+  else
+    puts "----------------------------------"
+    puts "Uh Oh - That wasn't the right one!"
+    puts "----------------------------------"
+    puts "Please try again, you've got this!"
+    puts "----------------------------------"
+  end
+end
+
+puts "That's the end for now, better luck next time!"
+
